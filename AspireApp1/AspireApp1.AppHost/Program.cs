92818cd2b224dc3d97c7648521df274cd5930329
Efim Zabarsky <EZ>
2024-05-05 09:49:05 +0300

    rfct

--- AspireApp1/AspireApp1.AppHost/Program.cs
+++ AspireApp1/AspireApp1.AppHost/Program.cs
@@ -5,6 +5,7 @@ var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
 
 builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
 
